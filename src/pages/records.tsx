381de1ceadfa19cf37c4c0b03713e455aba30726
the_liolik <85246338+liolikus@users.noreply.github.com>
2023-04-12 16:13:28 +0300

    Update records.tsx

--- src/pages/records.tsx
+++ src/pages/records.tsx
@@ -51,9 +51,9 @@ const RecordsPage: NextPageWithLayout = () => {
           bgColor="bg-white shadow-card dark:bg-light-dark"
         >
           &bull; {' '}
-          <a href="https://demoxlabs.xyz">English</a>
+          <a href="https://medium.com/@alex.brunko/my-first-aleo-dapp-creating-onchain-activities-in-tesnet-3-1abc11554e9f">English</a>
           &bull; {' '}
-          <a href="https://demoxlabs.xyz">Russian</a>
+          <a href="https://medium.com/@alex.brunko/%D0%BF%D0%B5%D1%80%D0%B2%D0%BE%D0%B5-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D0%B0%D0%BB%D0%B5%D0%BE-%D0%B8-%D0%B4%D0%BE%D1%81%D1%82%D1%83%D0%BF%D0%BD%D1%8B%D0%B5-%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D0%B8-%D0%B2-%D1%82%D0%B5%D1%81%D0%BD%D0%B5%D1%82-3-d0a849569224">Russian</a>
         </Section>
         <Section
           title="Environment and Leo first run | testnet_3"
